1e42242ad9d8a4813300475c5dced48714975e90
espalinha <matheusespalaor14@gmail.com>
2025-07-30 08:06:51 -0300

    Correção no config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import sys
 
 def dotenv_loader(path):
     with open("./src/loader/.env", "w") as f:
-        f.write(f'TRAIN={os.path.join(path,"WIDER_train/images/0--Parade")}')
-        f.write(f'TEST={os.path.join(path,"WIDER_test/images/0--Parade")}') 
-        f.write(f'TRAIN_ANNOT={os.path.join(path,"wider_face_split/wider_face_train_bbx_gt.txt")}')
-        f.write(f'VAL_ANNOT={os.path.join(path,"wider_face_split/wider_face_val_bbx_gt.txt")}') 
-        f.write(f'TEST_ANNOT={os.path.join(path,"wider_face_split/wider_face_test_bbx_gt.txt")}')
-        f.write(f'PRE={os.path.join(path)}') 
-        f.write(f'PRE_TRAIN={os.path.join(path,"WIDER_train/images/")}') 
-        f.write(f'PRE_VAL={os.path.join(path,"WIDER_val/images/")}') 
+        f.write(f'TRAIN={os.path.join(path,"WIDER_train/images/0--Parade")}\n')
+        f.write(f'TEST={os.path.join(path,"WIDER_test/images/0--Parade")}\n') 
+        f.write(f'TRAIN_ANNOT={os.path.join(path,"wider_face_split/wider_face_train_bbx_gt.txt")}\n')
+        f.write(f'VAL_ANNOT={os.path.join(path,"wider_face_split/wider_face_val_bbx_gt.txt")}\n') 
+        f.write(f'TEST_ANNOT={os.path.join(path,"wider_face_split/wider_face_test_bbx_gt.txt")}\n')
+        f.write(f'PRE={os.path.join(path)}\n') 
+        f.write(f'PRE_TRAIN={os.path.join(path,"WIDER_train/images/")}\n') 
+        f.write(f'PRE_VAL={os.path.join(path,"WIDER_val/images/")}\n') 
 
 def dotenv_yolo(path):
     with open("./src/yolo/.env", "w") as f:
